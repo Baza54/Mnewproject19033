fc74603a5c0e6c1d0cf44f98728e2c695940b322
Baza54 <nerf.king.neon@mail.ru>
2022-03-19 14:33:56 +0700

    Новый коммит5

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -22,7 +22,7 @@ public class MainActivity extends AppCompatActivity {
 
     private AppBarConfiguration appBarConfiguration;
     private ActivityMainBinding binding;
-//аф12222222344
+//аф122222223445
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
